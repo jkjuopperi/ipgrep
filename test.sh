191d3ccd7435ce8f4a7de21529f43b4231f0a306
Joonas Kuorilehto <joneskoo@kapsi.fi>
2011-11-26 16:31:36 +0200

    More tests

--- test.sh
+++ test.sh
@@ -3,3 +3,9 @@
 ./ipgrep 1.2.3.5/24 test.txt
 
 ./ipgrep 1.2.3.5/24 test.txt test2.txt
+
+echo 4.3.2.1/24
+./ipgrep 4.3.2.1/24 test.txt test2.txt
+
+echo 1.2.3.4
+./ipgrep 1.2.3.4 test.txt test2.txt
